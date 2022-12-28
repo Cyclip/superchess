413356592b3c67b26abf963700a3546f7184f581
Ifaz Ahmed <gdneop@gmail.com>
2022-12-28 17:38:52 +0000

    Create chess mechanics
    They are likely faulty but we'll test them later!

---
+++ chess/chess.py
@@ -0,0 +1,180 @@
+import numpy as np
+import pieces as chess_pieces
+import colorama
+
+import random
+import time
+import os
+
+class ChessBoard:
+    """
+    A chessboard is 16x16 represented as an array of pieces.
+    The columns are labeled A-P
+    Rows are labeled 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, A, B, C, D, E, F
+    Piece characters are as follows:
+        K - King ♔         ∞
+        Q - Queen ♕        8
+        W - Wormhole ⇆     7
+        R - Rook ♖         5
+        B - Bishop ♗       3
+        N - Knight ♘       3
+        P - Pawn ♙         1
+    Color characters are as follows:
+        W - White
+        B - Black
+    """
+    def __init__(self):
+        self.board = chess_pieces.gen_board()
+        self.shape = self.board.shape
+        self.last_moved_piece = None
+        self.last_moved_piece_from = None
+        self.last_moved_piece_to = None
+    
+    def __repr__(self):
+        return self.board_to_string()
+    
+    def board_to_string(self):
+        """
+        Returns a string representation of the board.
+        """
+        # Prints all the pieces in a nice grid
+        # Black pieces are printed in red
+        # White pieces are printed in white
+        # Last moved piece is printed in green
+        # Moves from are printed in yellow background
+
+        board_string = ""
+
+        # Add column labels
+        board_string += "   "
+        for i in range(16):
+            board_string += chr(i + 65) + " "
+        board_string += "\n"
+
+        for i, row in enumerate(self.board):
+            # Add row labels
+            board_string += f"{i+1:>2} "
+            for j, piece in enumerate(row):
+                if piece:
+                    # Colour
+                    if piece == self.last_moved_piece:
+                        board_string += colorama.Fore.GREEN
+                    elif piece.colour == "B":
+                        board_string += colorama.Fore.RED
+                    else:
+                        board_string += colorama.Fore.WHITE
+
+                    # Piece symbol
+                    board_string += piece.__repr__()
+                    
+                    # Reset colour
+                    board_string += colorama.Fore.RESET
+                    board_string += colorama.Back.RESET
+                elif (i, j) == self.last_moved_piece_from:
+                    board_string += colorama.Back.BLACK
+                    board_string += " "
+                    board_string += colorama.Back.RESET
+                else:
+                    board_string += " "
+                
+                board_string += " " + colorama.Fore.RESET
+            board_string += "\n"
+        
+        return board_string[:-1]
+    
+    def get_piece(self, pos):
+        """
+        Returns the piece at the given position.
+        """
+        try:
+            return self.board[pos[0]][pos[1]]
+        except IndexError:
+            return None
+
+ 
+    def is_empty(self, pos):
+        """
+        Returns True if the given position is empty.
+        """
+        return self.get_piece(pos) is None
+    
+    def set_piece(self, pos, piece):
+        """
+        Sets the piece at the given position.
+        No checking is done to ensure the move is legal.
+        """
+        self.board[pos[0]][pos[1]] = piece
+    
+    def move_piece(self, start_pos, end_pos):
+        """Move a piece without checking for legality."""
+        piece = self.get_piece(start_pos)
+        self.set_piece(start_pos, None)
+        self.set_piece(end_pos, piece)
+        piece.moved = True
+
+        self.last_moved_piece = piece
+        self.last_moved_piece_from = tuple(start_pos)
+        self.last_moved_piece_to = tuple(end_pos)
+
+        # May be a pawn moving 2 spaces
+        if isinstance(piece, chess_pieces.Pawn):
+            piece.moved_two = abs(start_pos[0] - end_pos[0]) == 2
+    
+    def get_legal_moves(self, pos):
+        """
+        Returns a list of legal moves for the piece at the given position.
+        """
+        piece = self.get_piece(pos)
+        if not piece:
+            return []
+        
+        moves = piece.get_moves(self)
+        legal_moves = []
+        for move in moves:
+            if self.is_legal_move(piece, move):
+                legal_moves.append(move)
+        
+        return legal_moves
+    
+    def is_legal_move(self, piece, move):
+        """
+        Returns True if the given move is legal for the given piece.
+        Revealing your king is considered a legal move, just a very stupid blunder.
+        I mean we give so much help by preventing that move, it's only fair.
+        """
+        # Global rules apply to every single piece
+        # Move is illegal if it attempts to move off the board
+        # Based on self.shape
+        if move[0] < 0 or move[0] >= self.shape[0]:
+            return False
+        if move[1] < 0 or move[1] >= self.shape[1]:
+            return False
+        
+        # Move is illegal if it attempts to move onto a friendly piece
+        if self.get_piece(move):
+            if self.get_piece(move).colour == piece.colour:
+                return False
+            
+        return True
+    
+    def get_white_pieces(self):
+        """
+        Returns a list of all white pieces.
+        """
+        white_pieces = filter(
+            lambda piece: piece and piece.colour == "W",
+            np.array(self.board).flatten()
+        )
+        
+        return np.array(list(white_pieces))
+    
+    def get_black_pieces(self):
+        """
+        Returns a list of all black pieces.
+        """
+        black_pieces = filter(
+            lambda piece: piece and piece.colour == "B",
+            np.array(self.board).flatten()
+        )
+        
+        return np.array(list(black_pieces))
---
+++ chess/pieces.py
@@ -0,0 +1,333 @@
+import numpy as np
+import colorama
+
+class Piece:
+    def __init__(self, colour, pos):
+        self.colour = colour
+        self.pos = pos
+        self.has_moved = False
+        self.symbol = ' '
+    
+    def colour_code(self, symbol):
+        if self.colour == "W":
+            return colorama.Fore.WHITE + symbol + colorama.Fore.RESET
+        else:
+            return colorama.Fore.RED + symbol + colorama.Fore.RESET
+
+    def __repr__(self):
+        return self.symbol
+
+class King(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 1000
+        self.symbol = "♔"
+    
+    def get_moves(self, board):
+        """
+        Can move one space in any direction.
+        """
+        moves = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    continue
+                moves.append([self.pos[0] + i, self.pos[1] + j])
+        
+        return moves
+
+class Queen(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 9
+        self.symbol = "♕"
+    
+    def get_moves(self, board):
+        """
+        Can move any number of spaces in any direction.
+        Once it hits a piece, it cannot move past it.
+        """
+        moves = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    continue
+                    
+                direction = [i, j]
+
+                # Move in direction until it hits a piece
+                for k in range(16):
+                    pos = [self.pos[0] + direction[0] * (k + 1), self.pos[1] + direction[1] * (k + 1)]
+                    piece = board.get_piece(pos)
+                    if piece:
+                        if piece.colour != self.colour:
+                            # Can capture enemy piece
+                            moves.append(pos)
+                        # Cannot move past any piece
+                        break
+                    else:
+                        # Can move to empty space
+                        moves.append(pos)
+        
+        return moves
+    
+class Wormhole(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 7
+        self.symbol = "⇆"
+    
+    def get_moves(self, board):
+        """
+        Movement rules:
+        1. Can move 1 step in any direction
+        2. Can loop round the board
+            a. Only on left and right sides
+        3. Can jump over friendly pawns
+            a. Destination *must* be empty
+            b. Can only jump horizontally or vertically
+        """
+        # Rule 1: Move 1 step in any direction
+        moves = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    continue
+
+                pos = [self.pos[0] + i, self.pos[1] + j]
+
+                # Rule 3: If it is on a pawn, it can jump over it
+                # Is it on a pawn?
+                on = board.get_piece(pos)
+                if isinstance(on, Pawn):
+                    # Is it a friendly pawn?
+                    if on.colour == self.colour:
+                        # Rule 3a: Destination (dir * 2) must be empty
+                        direction = [i, j]
+                        dest = [pos[0] + direction[0], pos[1] + direction[1]]
+                        if board.get_piece(dest) is None:
+                            # Rule 3b: Can only jump horizontally or vertically
+                            if direction[0] == 0 or direction[1] == 0:
+                                moves.append(dest)
+                    else:
+                        # Can capture enemy pawn
+                        moves.append(pos)
+                else:
+                    # Can move to empty space or capture enemy piece
+                    moves.append(pos)
+        
+        # Rule 2: Loop round the board
+        # Position in form (y, x)
+        for i in range(len(moves)):
+            # If moving out of through right side
+            if moves[i][1] > 15:
+                # Teleport to left side
+                moves[i][1] = 0
+            # If moving out of through left side
+            elif moves[i][1] < 0:
+                # Teleport to right side
+                moves[i][1] = 15
+        
+        return moves
+
+class Rook(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 5
+        self.symbol = "♖"
+    
+    def get_moves(self, board):
+        """
+        Can move any number of spaces in any direction.
+        Once it hits a piece, it cannot move past it.
+        """
+        directions = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1]
+        ]
+        moves = []
+        for direction in directions:
+            # Move in direction until it hits a piece
+            for k in range(16):
+                pos = [self.pos[0] + direction[0] * (k + 1), self.pos[1] + direction[1] * (k + 1)]
+                piece = board.get_piece(pos)
+                if piece:
+                    if piece.colour != self.colour:
+                        # Can capture enemy piece
+                        moves.append(pos)
+                    # Cannot move past any piece
+                    break
+                else:
+                    # Can move to empty space
+                    moves.append(pos)
+        
+        return moves
+
+class Bishop(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 3
+        self.symbol = "♗"
+    
+    def get_moves(self, board):
+        """
+        Can move any number of spaces in any direction.
+        Once it hits a piece, it cannot move past it.
+        """
+        directions = [
+            [1, 1],
+            [-1, 1],
+            [1, -1],
+            [-1, -1]
+        ]
+        moves = []
+        for direction in directions:
+            # Move in direction until it hits a piece
+            for k in range(16):
+                pos = [self.pos[0] + direction[0] * (k + 1), self.pos[1] + direction[1] * (k + 1)]
+                piece = board.get_piece(pos)
+                if piece:
+                    if piece.colour != self.colour:
+                        # Can capture enemy piece
+                        moves.append(pos)
+                    # Cannot move past any piece
+                    break
+                else:
+                    # Can move to empty space
+                    moves.append(pos)
+        
+        return moves
+
+class Knight(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 3
+        self.symbol = "♘"
+    
+    def get_moves(self, board):
+        # Positions are in format (y, x)
+        knight_moves = [
+            [2, 1],
+            [2, -1],
+            [-2, 1],
+            [-2, -1],
+            [1, 2],
+            [1, -2],
+            [-1, 2],
+            [-1, -2]
+        ]
+        moves = []
+        for move in knight_moves:
+            pos = [self.pos[0] + move[0], self.pos[1] + move[1]]
+            # we trust that the board will check if the move is valid
+            # through global rules
+            moves.append(pos)
+        
+        return moves
+
+class Pawn(Piece):
+    def __init__(self, colour, pos):
+        super().__init__(colour, pos)
+        self.value = 1
+        self.has_moved_two_spaces = False
+        self.symbol = "♙"
+    
+    def get_moves(self, board):
+        """
+        Rules:
+        1. Can move relatively forward one space
+        2. Can move relatively forward two spaces if it has not moved yet
+        3. Can capture diagonally forward one space
+        4. Can en-passant capture diagonally forward one space
+        """
+        moves = []
+
+        # Rule 1: Can move relatively forward one space
+        direction = 1 if self.colour == "B" else -1
+        pos = [self.pos[0] + direction, self.pos[1]]
+        if board.is_empty(pos):
+            moves.append(pos)
+            # Rule 2: Can move relatively forward two spaces if it has not moved yet
+            if not self.has_moved:
+                pos = [self.pos[0] + direction * 2, self.pos[1]]
+                if board.is_empty(pos):
+                    moves.append(pos)
+            
+        # Rule 3: Can capture diagonally forward one space
+        for i in [-1, 1]:
+            # Move up in direction, and either left or right
+            pos = [self.pos[0] + direction, self.pos[1] + i]
+            piece = board.get_piece(pos)
+            if piece and piece.colour != self.colour:
+                moves.append(pos)
+        
+        # Rule 4: Can en-passant capture diagonally forward one space
+        # Check if there is a pawn in same row, one space away
+        for i in [-1, 1]:
+            pos = [self.pos[0], self.pos[1] + i]
+            piece = board.get_piece(pos)
+            if isinstance(piece, Pawn) and piece.colour != self.colour:
+                # Check if the pawn has moved two spaces
+                if piece.has_moved_two_spaces:
+                    moves.append([self.pos[0] + direction, self.pos[1] + i])
+
+        return moves
+
+
+def symbol_to_piece(symbol, pos):
+    colour = "W" if symbol.isupper() else "B"
+    symbol = symbol.upper()
+    match symbol:
+        case "K":
+            return King(colour, pos)
+        case "Q":
+            return Queen(colour, pos)
+        case "W":
+            return Wormhole(colour, pos)
+        case "R":
+            return Rook(colour, pos)
+        case "B":
+            return Bishop(colour, pos)
+        case "N":
+            return Knight(colour, pos)
+        case "P":
+            return Pawn(colour, pos)
+        case " ":
+            return None
+        case _:
+            raise ValueError(f"Invalid symbol: {symbol}")
+
+
+def gen_board():    
+    # Position is (y, x)
+    black_pieces = [symbol_to_piece(piece.lower(), (0, i)) for i, piece in enumerate(pieces)]
+    black_pawns = [symbol_to_piece("p", (1, i)) for i in range(16)]
+
+    white_pawns = [symbol_to_piece("P", (14, i)) for i in range(16)]
+    white_pieces = [symbol_to_piece(piece, (15, i)) for i, piece in enumerate(pieces[::-1])]
+
+    board = np.array([
+        black_pieces,
+        black_pawns,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        [None] * 16,
+        white_pawns,
+        white_pieces,
+    ])
+
+    return board
+
+# Default position
+pieces = ['R', 'B', 'N', 'W', 'B', 'R', 'N', 'Q', 'K', 'N', 'R', 'B', 'W', 'N', 'B', 'R']
